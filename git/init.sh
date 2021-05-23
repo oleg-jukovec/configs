edfd2e381743f563c1d258842780d9aa3a231f24
Oleg Jukovec <oleg.jukovec@gmail.com>
2021-05-23 12:17:22 +0300

    Change initial editor for git to vim.

--- git/init.sh
+++ git/init.sh
@@ -2,6 +2,6 @@
 
 git config --global user.email "oleg.jukovec@gmail.com"
 git config --global user.name "Oleg Jukovec"
-git config --global core.editor "nano"
+git config --global core.editor "vim"
 git config --global push.default simple
 git config --global core.pager 'less -x1,5'
